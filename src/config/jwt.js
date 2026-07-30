244fec44beac4c724ea8084a1a1ff4ed6646e5fe
akbar saputra <barltzyml@gmail.com>
2026-07-30 12:58:12 +0900

    m

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const JWT_SECRET = process.env.JWT_SECRET
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d"
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "30d"
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET belum diatur di file .env")
